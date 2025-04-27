2c198eb6f492643b7700f3cce96f87528e0fcb1c
Joseph Andre Balbada <josephandrebalbada@gmail.com>
2025-04-27 15:50:24 +0800

    hdfh

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -37,14 +37,14 @@
     <link href="{{ secure_asset('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
     <link href="{{ secure_asset('assets/css/style.css') }}" rel="stylesheet"> --}}
 
-    <link href="{{ secure_asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('assets/css/style.css') }}" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+    <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
     
     <!-- Corrected Favicon Link -->
     <link rel="icon" href="{{ secure_asset('assets/img/favicon.png') }}" type="image/png">    
