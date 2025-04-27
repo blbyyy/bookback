f46599d2802a344e3a99f6a808cbebf5c9743825
Joseph Andre Balbada <josephandrebalbada@gmail.com>
2025-04-28 05:58:29 +0800

    sfasfasfa

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,22 +11,24 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="{{ asset('assets/img/bookback.png') }}" rel="icon">
-  <link href="{{ asset('assets/img/bookback.png') }}" rel="apple-touch-icon">
+  {{-- <link href="{{ asset('assets/img/bookback.png') }}" rel="icon">
+  <link href="{{ asset('assets/img/bookback.png') }}" rel="apple-touch-icon"> --}}
+  <link href="{{ secure_asset('assets/img/bookback.png') }}" rel="icon">
+  <link href="{{ secure_asset('assets/img/bookback.png') }}" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-    <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet"> --}}
     
 
     <link href="{{ secure_asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
@@ -306,7 +308,7 @@
   <!-- Vendor JS Files -->
   <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
-  <script src="{{ asset('assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
+  {{-- <script src="{{ asset('assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
   <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
   <script src="{{ asset('assets/vendor/chart.js/chart.umd.js')}}"></script>
   <script src="{{ asset('assets/vendor/echarts/echarts.min.js')}}"></script>
@@ -315,7 +317,7 @@
   <script src="{{ asset('assets/vendor/tinymce/tinymce.min.js')}}"></script>
   <script src="{{ asset('assets/vendor/php-email-form/validate.js')}}"></script>
   <script src="{{ asset('assets/js/main.js')}}"></script>
-  <script src="{{ asset('js/custom.js')}}"></script>
+  <script src="{{ asset('js/custom.js')}}"></script> --}}
 
   <script src="{{ secure_asset('assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
   <script src="{{ secure_asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
