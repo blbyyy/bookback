d5789b583dd15455cce940d956392f38a1c4150b
Joseph Andre Balbada <josephandrebalbada@gmail.com>
2025-04-27 15:43:12 +0800

    sfas

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -19,7 +19,7 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-    <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
@@ -35,7 +35,17 @@
     <link href="{{ secure_asset('assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
     <link href="{{ secure_asset('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
     <link href="{{ secure_asset('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('assets/css/style.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/css/style.css') }}" rel="stylesheet"> --}}
+
+    <link href="https://bookback-bookback.ibrtwo.easypanel.host/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="https://bookback-bookback.ibrtwo.easypanel.host/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet" type="text/css" />
+    <link href="https://bookback-bookback.ibrtwo.easypanel.host/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet" type="text/css" />
+    <link href="https://bookback-bookback.ibrtwo.easypanel.host/assets/vendor/quill/quill.snow.css" rel="stylesheet" type="text/css" />
+    <link href="https://bookback-bookback.ibrtwo.easypanel.host/assets/vendor/quill/quill.bubble.css" rel="stylesheet" type="text/css" />
+    <link href="https://bookback-bookback.ibrtwo.easypanel.host/assets/vendor/remixicon/remixicon.css" rel="stylesheet" type="text/css" />
+    <link href="https://bookback-bookback.ibrtwo.easypanel.host/assets/vendor/simple-datatables/style.css" rel="stylesheet" type="text/css" />
+    <link href="https://bookback-bookback.ibrtwo.easypanel.host/assets/css/style.css" rel="stylesheet" type="text/css" />
+
   <!-- Template Main CSS File -->
     
 
